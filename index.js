271660e482a5af72989b08366a2bdcc5e48f9343
matt-colen <mzcolen@gmail.com>
2023-10-22 20:10:34 -0500

    Refactors code
    Refactors code for more clarity.

--- index.js
+++ index.js
@@ -1,65 +1,85 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+  databaseURL: "https://add-to-cart-5197b-default-rtdb.firebaseio.com/",
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const shoppingListEl = document.getElementById("shopping-list")
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    push(shoppingListInDB, inputValue)
-    
-    clearInputFieldEl()
-})
+// Listener for enter keypress on inputFieldEl
+inputFieldEl.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    updateDatabase();
+  }
+});
 
-onValue(shoppingListInDB, function(snapshot) {
-    if (snapshot.exists()) {
-        let itemsArray = Object.entries(snapshot.val())
-    
-        clearShoppingListEl()
-        
-        for (let i = 0; i < itemsArray.length; i++) {
-            let currentItem = itemsArray[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
-            
-            appendItemToShoppingListEl(currentItem)
-        }    
-    } else {
-        shoppingListEl.innerHTML = "No items here... yet"
+// Listener for click on Add To Cart Button
+addButtonEl.addEventListener("click", () => {
+  updateDatabase();
+});
+
+const updateDatabase = () => {
+  let inputValue = inputFieldEl.value;
+
+  if (inputValue) {
+    push(shoppingListInDB, inputValue);
+
+    clearInputFieldEl();
+  }
+};
+
+onValue(shoppingListInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
+
+    clearShoppingListEl();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
+
+      appendItemToShoppingListEl(currentItem);
     }
-})
+  } else {
+    shoppingListEl.innerHTML = "No items here... yet";
+  }
+});
 
-function clearShoppingListEl() {
-    shoppingListEl.innerHTML = ""
-}
+const clearShoppingListEl = () => {
+  shoppingListEl.innerHTML = "";
+};
 
-function clearInputFieldEl() {
-    inputFieldEl.value = ""
-}
+const clearInputFieldEl = () => {
+  inputFieldEl.value = "";
+};
 
-function appendItemToShoppingListEl(item) {
-    let itemID = item[0]
-    let itemValue = item[1]
-    
-    let newEl = document.createElement("li")
-    
-    newEl.textContent = itemValue
-    
-    newEl.addEventListener("click", function() {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        
-        remove(exactLocationOfItemInDB)
-    })
-    
-    shoppingListEl.append(newEl)
-}
+const appendItemToShoppingListEl = (item) => {
+  let itemID = item[0];
+  let itemValue = item[1];
+
+  let newEl = document.createElement("li");
+
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("click", () => {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+
+    remove(exactLocationOfItemInDB);
+  });
+
+  shoppingListEl.append(newEl);
+};
